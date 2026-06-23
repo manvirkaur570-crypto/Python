1c776fa62506c5ee43e1514365c7cd079753c3b5
manvir-01 <manvirkaur570@gmail.com>
2026-06-23 15:46:33 +0530

    Add files via upload

---
+++ for.py
@@ -0,0 +1,40 @@
+# for loop -- c or c++
+# for(int i=0;i<5;i++){
+    # ////
+# }
+
+# for i in range(0,5):   # stop -- stop-1 
+#      print(i)
+# for i in range(0,5):   # stop -- stop-1 
+#      print("Karan",i)
+    
+#for i in range(1,10,1):  # start stop step 
+#     print(i)
+#for i in range(1,10,2):  # start stop step -- skip one
+#     print(i)
+#for i in range(1,10,3):  # start stop step 
+#     print(i)
+
+# for i in range(10,1,-1):  # start stop step stop-1-- 1-(-1)=2
+#     print(i)
+# for i in range(10,1,-2):  # start stop step stop-1-- 1-(-1)=2
+#       print(i)
+
+
+# number=int(input("Enter number ")) # multipliation table
+# upto=int(input("Enter Upto "))
+# for i in range(1,upto+1):
+#      print(number,"*",i,"=",number*i)
+    
+
+# *
+# **
+# ***
+# ****
+# *****
+# ******
+
+#for i in range(1,7):  # i=3
+#     for j in range(0,i):  # 0 to 3
+#         print("*",end="")
+#     print("")

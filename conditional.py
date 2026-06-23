640d41f0f8b3d9ea8b9d1da7890c416c54fad17c
manvir-01 <manvirkaur570@gmail.com>
2026-06-23 15:45:42 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,26 @@
+# Conditions
+
+# a=18
+# if(a<18):
+#  print("Eligible for voting")
+# else: 
+#  print("Not Eligible for voting")
+
+
+# to check whether the number is odd or even
+
+# a= int(input("Enter your number: "))
+# if(a%2==0):
+#  print(f"{a} is Even Number")
+# else:
+#  print(a,"is the odd Number")
+
+
+# if- elif Statements
+# a=int(input("Enter your age:"))
+# if(a<=12):
+#     print("You are a child")
+# elif(a<18):
+#     print("You are a teenager")
+# else:
+#     print("You are an adult")

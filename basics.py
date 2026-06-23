bf630f08a4a6006e14db9442181afa6ac5060308
manvir-01 <manvirkaur570@gmail.com>
2026-06-23 15:45:05 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,41 @@
+# print("hello")
+
+# print('hi',end="")
+
+#Variables and data type
+#print('a')
+# a=1.3
+# print(a)
+
+#b=5.7
+#print(b)
+
+# c=True 
+# print(c)
+# print(type(c))
+
+#Data type conversion
+# a=4.89
+# print(int(a))
+
+# a=5
+# print(float(a))
+
+# name= karam
+# print(int(name))
+# print(type(a))
+
+# b="Karam"
+# print(b)
+# print(type(b))
+
+# user input 
+# a=int(input("Enter a "))  # by default string input 
+# b=int(input("enter b "))
+#print("the value of a is",a)
+# print(a+b)
+
+# c=5
+# d=6
+# print("the value of c is",c)
+# print(c+d)

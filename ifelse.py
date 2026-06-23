7aca49829f7c1462de75d8fe7e400d99b181a28c
manvir-01 <manvirkaur570@gmail.com>
2026-06-23 15:46:14 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,25 @@
+#If-else -- nested if 
+
+# marks=int(input("Enter Marks "))
+# if marks>=90:
+#      if marks>100:
+#          print("Invalid")
+#      else: 
+#          print("A")
+# elif marks>=80:
+#      print("B")
+# elif marks>=70:
+#      print("C")
+# elif marks>=60:
+#      print("D")
+# elif marks<60:
+#      print("Fail")
+
+
+ # ternary operator 
+
+# a=4
+# ternary = valueiftrue if else valueiffalse --syntax
+# answer = "Even" if a%2==0 else "Odd"
+# answer = print("Even") if a%2==0 else print("Odd") 
+# print(answer)

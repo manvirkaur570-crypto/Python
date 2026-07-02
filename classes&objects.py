0b7636944756cf7bd1d3a5f1d909b361ce274f4b
manvir-01 <manvirkaur570@gmail.com>
2026-07-02 16:22:35 +0530

    Add files via upload

---
+++ classes&objects.py
@@ -0,0 +1,49 @@
+# class aand object 
+# Python constructor is an instance method in a class, that is automatically called whenever a new object of the class is created. The constructor's role is to assign value to instance variables as soon as the object is declared.
+
+# class students:
+#      print("Hello this is students class")
+#      def __init__(self,n,r):
+#       print("this is constructor")
+#       self.naam=n 
+#       self.roll=r
+#     #  naam=n 
+#     #  roll=r
+#      print(f"name is {self.naam} and roll is {self.roll} ")
+#      def __init__(self):
+#          pass
+#      def getValues(self):
+#          self.name= input("Enter your name ")
+# #          self.rollno= input("Enter your rollnumber ")
+#      def printVal(self):
+#          print(f"name is {self.name} and roll is {self.rollno}")
+        
+        
+        
+# stu1= students()
+# stu1= students('Manvir',12)
+# print(stu1)
+# stu2= students('Mohit',34)
+
+# stu1.printVal()
+
+
+# stu1= students()
+# stu1.getValues()
+
+# stu2= students()
+# stu2.getValues()
+
+# stu2.printVal()
+
+#
+# class Car:
+#     def __init__(self,name,brand):
+#         self.name = name
+#         self.brand = brand
+
+#     def display(self):
+#         print(f"Name is {self.name} and brand is {self.brand}")
+
+# car= Car("Fortuner","Toyota")
+# car.display()
